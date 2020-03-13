e19071839b7cbc26c8d90bca7bf38ab44638e476
rfaelfp <rafaelfepedrosa@gmail.com>
2020-03-13 10:38:43 -0300

    Atualização 13/03/2020

--- ConsoleApp1/ConsoleApp1/Exercício 1.cs
+++ ConsoleApp1/ConsoleApp1/Exercício 1.cs
@@ -4,6 +4,12 @@ public class Program
 {
 	public static void Main()
 	{
+
+		Program.Exercicio01();
+		Program.Exercicio02();
+	}
+	static void Exercicio01()
+	{
 		int numero1, numero2, numero3, numero4, soma;
 
 		Console.WriteLine("Programa que soma quatro números");
@@ -28,4 +34,22 @@ public class Program
 
 		Console.WriteLine("Soma = " + soma);
 	}
+
+	static void Exercicio02()
+	{
+		float nota1, nota2, nota3, mediaAritimetica;
+
+		Console.WriteLine("Programa de média aritimética");
+
+		Console.Write("Digite a primeira nota ");
+		nota1 = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Digite a segunda nota ");
+		nota2 = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Digite a terceira nota ");
+		nota3 = Convert.ToInt32(Console.ReadLine());
+
+		mediaAritimetica = (nota1 + nota2 + nota3) / 3;
+		Console.WriteLine($"A média aritimética é {mediaAritimetica.ToString("F2")}");
+	}
 }
+
