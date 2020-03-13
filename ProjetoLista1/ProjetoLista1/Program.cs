e166f0cbf441591cb1e839196b4419789f127d3c
rfaelfp <rafaelfepedrosa@gmail.com>
2020-03-13 18:06:01 -0300

    13/03/2020

--- ProjetoLista1/ProjetoLista1/Program.cs
+++ ProjetoLista1/ProjetoLista1/Program.cs
@@ -88,6 +88,126 @@ namespace ProjetoLista1
 			Console.WriteLine(valorImposto);
 			Console.WriteLine(precoFinal.ToString("C2"));
 			}
+			else if (exercicioEscolha == 12)
+			{
+				double alturaDegrau, alturaASerAlcancada, quantidadeDegraus;
+				Console.Write("Digite a altura do degrau: ");
+				alturaDegrau = Convert.ToDouble(Console.ReadLine());
+				Console.Write("Digite a altura que deseja alcançar: ");
+				alturaASerAlcancada = Convert.ToDouble(Console.ReadLine());
+
+				quantidadeDegraus = alturaDegrau / alturaASerAlcancada;
+				Console.WriteLine($"Será necessário subir {quantidadeDegraus} degraus.");
+			}
+			else if (exercicioEscolha == 13)
+			{
+				double anguloGrauEscada, alturaParede, radiano, hipotenusa, resultado;
+				Console.Write("Indique o angulo em graus da escada: ");
+				anguloGrauEscada = Convert.ToDouble(Console.ReadLine());
+				Console.Write("Indique o altura da parede: ");
+				alturaParede = Convert.ToDouble(Console.ReadLine());
+				radiano = anguloGrauEscada * (Math.PI / 180);
+				hipotenusa = Math.Cos(radiano);
+				resultado = alturaParede / hipotenusa;
+				Console.Write(resultado);
+			}
+			else if (exercicioEscolha == 14)
+			{
+				double numero, parteInteira, parteFracionada, arredondado;
+				Console.WriteLine("Digite o número desejado: ");
+				numero = Convert.ToDouble(Console.ReadLine());
+				parteInteira = Math.Floor(numero);
+				Console.Write(parteInteira);
+			}//voltar nesse exercício
+			else if (exercicioEscolha == 15)
+			{
+				int hora, minuto, guardaHora, resultado;
+				Console.Write("Digite a hora: ");
+				hora = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Digite os minutos: ");
+				minuto = Convert.ToInt32(Console.ReadLine());
+				guardaHora = hora * 60;
+				resultado = guardaHora + minuto;
+				Console.WriteLine(resultado);
+			}
+			else if (exercicioEscolha == 16)
+			{
+				double custoTeatroTotal, precoConvite, qtdConvites;
+				Console.Write("Digite o custo do teatro: ");
+				custoTeatroTotal = Convert.ToDouble(Console.ReadLine());
+				Console.Write("Digite o preço do convite: ");
+				precoConvite = Convert.ToDouble(Console.ReadLine());
+				qtdConvites = custoTeatroTotal / precoConvite;
+				qtdConvites = Math.Ceiling(qtdConvites);
+				Console.WriteLine($"É necessário vender {qtdConvites} para pagar o custo do teatro.");
+			}
+			else if (exercicioEscolha == 17)
+
+			{
+				int i, numeroBase, resultado;
+				Console.WriteLine("Programa tabuada");
+				Console.Write("Digite o numero a ser multiplicado: ");
+				numeroBase = Convert.ToInt32(Console.ReadLine());
+				for (i = 1; i <= 10; i++)
+				{
+					resultado = i * numeroBase;
+					Console.WriteLine($"{numeroBase} X {i} = {resultado}");
+				}
+			}
+			else if (exercicioEscolha == 18)
+			{
+			DateTime dataAtual = DateTime.Now;
+			string ano = dataAtual.Year.ToString();
+			int anoAtualConvertido = Convert.ToInt32(ano), anoNascimento, anos;
+				double meses, dias, semanas;
+				Console.WriteLine("Digite o ano de nascimento: ");
+				anoNascimento = Convert.ToInt32(Console.ReadLine());
+				anos = anoAtualConvertido - anoNascimento;
+				Console.WriteLine(anos + " anos");
+				meses = 12 * anos;
+				Console.WriteLine(meses + " meses");
+				dias = anos * 365;
+				Console.WriteLine(dias + " dias");
+				semanas = anos * 52.1429;
+				Console.WriteLine(semanas.ToString("F2") + " semanas");
+			}
+			else if (exercicioEscolha == 19)
+			{
+				double catetoX, catetoY, hipotenusa, resultado;
+				Console.Write("Digite a altura do cateto: ");
+				catetoX = Convert.ToDouble(Console.ReadLine());
+				Console.Write("Digite a largura do cateto: ");
+				catetoY = Convert.ToDouble(Console.ReadLine());
+				resultado = Math.Pow(catetoX, 2) + Math.Pow(catetoY, 2);
+				hipotenusa = Math.Sqrt(resultado);
+				Console.WriteLine($"O valor da hipotenusa é {hipotenusa.ToString("F2")}");
+
+			}
+			else if (exercicioEscolha == 20)
+			{
+				double raio, cumprimentoEsfera, areaDaEsfera, volumeDaEsfera;
+				Console.Write("Digite o tamanho do Raio: ");
+				raio = Convert.ToDouble(Console.ReadLine());
+				cumprimentoEsfera = 2 * (Math.PI) * raio;
+				areaDaEsfera = 3.14 * Math.Pow(raio, 2);
+				volumeDaEsfera = 1/4 * (Math.PI) * raio;
+
+				Console.WriteLine($"O cumprimento da esfera é {cumprimentoEsfera}");
+				Console.WriteLine($"A área da esfera é {areaDaEsfera}");
+				Console.WriteLine($"O volume da esfera é {volumeDaEsfera}");
+			}// voltar porque não está dando para dividir 1/4
+			else if (exercicioEscolha == 21)
+			{
+				double dimensaoA, dimensaoB, totalMetrosQuadrados, potenciaUtilizada;
+				Console.WriteLine("Digite a primeira dimensão em m²: ");
+				dimensaoA = Convert.ToDouble(Console.ReadLine());
+				Console.WriteLine("Digite a segunda dimensão em m²: ");
+				dimensaoB = Convert.ToDouble(Console.ReadLine());
+
+				totalMetrosQuadrados = dimensaoA * dimensaoB;
+				potenciaUtilizada = totalMetrosQuadrados * 18;
+				Console.WriteLine($"O espaço possui {totalMetrosQuadrados}m² sendo necessário utilizar {potenciaUtilizada} W para iluminar.");
+			}
 			}
 		}
 }
