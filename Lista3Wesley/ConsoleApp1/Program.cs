647be250d821aa55294cb99792be970bd3590e05
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-03-27 13:46:47 -0300

    27/03/2020

--- Lista3Wesley/ConsoleApp1/Program.cs
+++ Lista3Wesley/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int questaoNumero;
-            Console.WriteLine("Escolha a questão: ");
+            Console.Write("Escolha a questão: ");
             questaoNumero = Convert.ToInt32(Console.ReadLine());
             if (questaoNumero == 1)
             {
@@ -120,7 +120,6 @@ namespace ConsoleApp1
             if (questaoNumero == 7)
             {
                 int numeroEntrada, i, resultado3 = 0, total3 = 0;
-                int[] multiplos = { 3, 5 };
                 
                 Console.Write("Escreva o número da entrada: ");
                 numeroEntrada = Convert.ToInt32(Console.ReadLine());
@@ -139,6 +138,53 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine($"Total da linha: {total3}");
                 //Console.WriteLine("[{0}]", string.Join(", ", multiplos3));
+            }
+            if (questaoNumero == 8)
+            {
+                int quantidade;
+                Console.Write("Digite a quantidade de pessoas que deseja incluir:");
+                quantidade = Convert.ToInt32(Console.ReadLine());
+                int[] sexo = new int[quantidade];
+                double[] altura = new double[quantidade];
+                int i, contadorMulheres = 0, contadorHomens = 0;
+                double maiorAltura = 0, menorAltura = double.MaxValue, alturaTotalMulheres = 0, AlturaTotalHomens = 0, mediaAlturaMulheres, mediaAlturaHomens;
+
+                for (i = 0; i < sexo.Length; i++)
+                {
+                    Console.Write("Digite o sexo (0 = feminino, 1 = masculino): ");
+                    sexo[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite a altura: ");
+                    altura[i] = Convert.ToDouble(Console.ReadLine());
+                    if (altura[i] > maiorAltura)
+                    {
+                        maiorAltura = altura[i];
+                    }
+                    if (altura[i] < menorAltura)
+                    {
+                        menorAltura = altura[i];
+                    }
+                    if (sexo[i] == 0)
+                    {
+                        alturaTotalMulheres += altura[i];
+                        contadorMulheres++;
+                    }
+                    else
+                    {
+                        AlturaTotalHomens += altura[i];
+                        contadorHomens++;
+                    }
+
+                }
+                mediaAlturaMulheres = alturaTotalMulheres / contadorMulheres;
+                mediaAlturaHomens = AlturaTotalHomens / contadorHomens;
+                Console.WriteLine($"A menor altura é {menorAltura}");
+                Console.WriteLine($"A maior altura é {maiorAltura}");
+                Console.WriteLine($"A média de altura das mulheres é {mediaAlturaMulheres}");
+                Console.WriteLine($"A média de altura dos homens é {mediaAlturaHomens}");
+                Console.WriteLine($"A média da pupulação é {(mediaAlturaMulheres + mediaAlturaHomens) / quantidade}");
+                Console.WriteLine($"O percentual de mulheres na população é {(contadorMulheres * 100) / quantidade}%");
+                Console.Write($"A percentual de homens da população é {(contadorHomens * 100) / quantidade }%");
+
             }
         }
     }
