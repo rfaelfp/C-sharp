3235706be7a712e43b19bfc8d63a81013b42f612
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-04-09 12:08:12 -0300

    09/04/2020

---
+++ Lista5Wesley/Questao1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool verificacao = true;
+            while (verificacao == true)
+            {
+                inverterNumero();
+                Console.WriteLine();
+            }
+
+
+        }
+        static void inverterNumero ()
+        {
+            int numero, i;
+            string numeroConvertido;
+            Console.Write("Digite um número para que ele seja invertido: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("O número invertido é: ");
+            numeroConvertido = numero.ToString();
+            for (i = numeroConvertido.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numeroConvertido[i]);
+            }
+        }
+    }
+}
---
+++ Lista5Wesley/Questao2/Questao2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool verificacao = true;
+            while (verificacao == true) 
+            { 
+            int num1 = 0, num2 = 0;
+            int RetornuNumero1 = pegarNumero(num1);
+            int RetornuNumero2 = pegarNumero(num2);
+            if (RetornuNumero2 != 0)
+                { 
+                int result = calculoNumero(RetornuNumero1, RetornuNumero2);
+                Console.WriteLine(result);
+                }
+            }
+        }
+        static int pegarNumero(int numero)
+        {
+            Console.Write("Infomer os números: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            return numero;
+        }
+        static int calculoNumero(int num1, int num2)
+        {
+            int piso = num1 / num2;
+            Convert.ToInt32(piso);
+            return num1 - (piso * num2);
+        }
+    }
+}
