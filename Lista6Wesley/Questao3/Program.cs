8f9b31290f1c008cb08f4b2231d21230c07cdee1
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-04-17 19:54:46 -0300

    17/04/2020

--- Lista6Wesley/Questao3/Program.cs
+++ Lista6Wesley/Questao3/Program.cs
@@ -10,72 +10,64 @@ namespace Questao3
             Console.Write("Digite o tamanho da matriz quadrada: ");
             tamanhoArray = Convert.ToInt32(Console.ReadLine());
             int[,] meuArray = new int[tamanhoArray, tamanhoArray];
-            int[] diagonalPrincial = new int[tamanhoArray];
-            int[] diagonalSecundaria = new int[tamanhoArray];
 
-            meuArray = getAleatorio(meuArray, tamanhoArray);
-
-            printMeuArray(meuArray, tamanhoArray);
-            Console.WriteLine();
-
-            diagonalPrincial = getDiagonalPrincial(meuArray, tamanhoArray, diagonalPrincial);
-            Console.Write("Linha diagonal principal: ");
-            printDiagonal(diagonalPrincial);
-
-            Console.WriteLine();
-            diagonalSecundaria = getDiagonalSecundaria(meuArray, tamanhoArray, diagonalSecundaria);
-            Console.Write("Linha diagonal secundária: ");
-            printDiagonal(diagonalSecundaria);
-
-            Console.WriteLine();
-            Console.WriteLine();
+            getAleatorio(meuArray);
+            printMeuArray(meuArray);
+            printDiagonal(getDiagonalPrincial(meuArray));
+            printDiagonal(getDiagonalSecundaria(meuArray));
         }
-        static int[,] getAleatorio(int[,] meuArray, int tamanhoArray)
+        static int[,] getAleatorio(int[,] meuArray)
         {
-            int i, j; 
+            int i, j;
             Random numeroAleatorio = new Random();
-            for (i = 0; i < tamanhoArray; i++)
-                for (j = 0; j < tamanhoArray; j++)
+            for (i = 0; i < meuArray.GetLength(0); i++)
+                for (j = 0; j < meuArray.GetLength(0); j++)
                     meuArray[i, j] = numeroAleatorio.Next(0, 1000);
             return meuArray;
         }
-        static void printMeuArray(int[,] meuArray, int tamanhoArray)
+        static void printMeuArray(int[,] meuArray)
         {
             int i, j;
-            for (i = 0; i < tamanhoArray; i++)
+            for (i = 0; i < meuArray.GetLength(0); i++)
             {
                 Console.WriteLine();
-                for (j = 0; j < tamanhoArray; j++)
+                for (j = 0; j < meuArray.GetLength(0); j++)
                     Console.Write(meuArray[i, j] + "\t");
             }
+            Console.WriteLine();
         }
-        static int[] getDiagonalPrincial(int[,] meuArray, int tamanhoArray, int[] diagonalPrincialParam)
+        static int[] getDiagonalPrincial(int[,] meuArray)
         {
+            Console.Write("Linha diagonal principal: ");
             int i, j, k = 0;
-            for (i = 0, j = 0; i < tamanhoArray; i++, j++)
+            int[] diagonalPrincial = new int[meuArray.GetLength(0)];
+            for (i = 0, j = 0; i < meuArray.GetLength(0); i++, j++)
 
-                {
-                        diagonalPrincialParam[k] = meuArray[i, j];
-                        k++;
-                }
-            return diagonalPrincialParam;
-        }
-        static int[] getDiagonalSecundaria(int[,] meuArray, int tamanhoArray, int[] diagonalSecundariaParam)
-        {
-            int i, j, k = 0;
-            for (i = 0, j = tamanhoArray - 1; i < tamanhoArray; i++, j--)
             {
-                diagonalSecundariaParam[k] = meuArray[i, j];
+                diagonalPrincial[k] = meuArray[i, j];
                 k++;
             }
-            return diagonalSecundariaParam;
+            return diagonalPrincial;
+        }
+        static int[] getDiagonalSecundaria(int[,] meuArray)
+        {
+            Console.Write("Linha diagonal secundária: ");
+            int i, j, k = 0;
+            int[] diagonalSecundaria = new int[meuArray.GetLength(0)];
+            for (i = 0, j = meuArray.GetLength(0) - 1; i < meuArray.GetLength(0); i++, j--)
+            {
+                diagonalSecundaria[k] = meuArray[i, j];
+                k++;
+            }
+            return diagonalSecundaria;
         }
         static void printDiagonal(int[] diagonal)
         {
-            foreach(int e in diagonal)
+            foreach (int e in diagonal)
             {
                 Console.Write(e + "\t");
             }
+            Console.WriteLine();
         }
     }
 }
