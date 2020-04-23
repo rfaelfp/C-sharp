dda8822c853198b3c932bf298ea90f4997d0b837
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-04-23 18:21:15 -0300

    23/04/2020

--- Lista5Wesley/Questao3/Program.cs
+++ Lista5Wesley/Questao3/Program.cs
@@ -6,7 +6,36 @@ namespace Questao3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n;
+            Console.Write("Digite o limite: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(metodoElevado(n));
+            Console.WriteLine(metodoSen(n));
+            Console.WriteLine(metodoLog(n));
+        }
+        static double metodoElevado(int n)
+        {
+            int i;
+            double soma = 0;
+            for (i = 0; i <= n; i++)
+                soma += Math.Sqrt(i);
+            return soma;
+        }
+        static double metodoSen(int n)
+        {
+            int i;
+            double soma = 0;
+            for (i = 0; i <= n; i++)
+                soma += Math.Sin(i + 5);
+            return soma;
+        }
+        static double metodoLog(int n)
+        {
+            int i;
+            double soma = 0;
+            for (i = 0; i <= n; i++)
+                soma += Math.Log10(Math.Pow(2, i));
+            return soma;
         }
     }
 }
---
+++ Lista5Wesley/Questao4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Digite o limete: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(metodoElevado(n));
+            Console.WriteLine(metodoLog(n));
+            Console.WriteLine(metodoCos(n));
+        }
+        static double metodoElevado(int n)
+        {
+            int i;
+            double multi = 1;
+            for (i = 1; i <= n; i++)
+                multi *= i / Math.Sqrt(i);
+            return multi;
+        }
+        static double metodoLog(int n)
+        {
+            int i;
+            double multi = 1;
+            for (i = 0; i <= n; i++)
+                multi *= Math.Exp(Math.Log(i + 12));
+            return multi;
+        }
+        static double metodoCos(int n)
+        {
+            int i;
+            double multi = 1;
+            for (i = 3; i <= n; i++)
+                multi *= Math.Pow(Math.Cos(i / 3), 4);
+            return multi;
+        }
+    }
+}
