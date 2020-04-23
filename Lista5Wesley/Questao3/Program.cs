418b05feb4ec9d48b9972644972b0c17f488d9ab
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-04-23 17:52:39 -0300

    23/04/2020

---
+++ Lista5Wesley/Questao3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lista5Wesley/Questao5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Questao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            Console.Write("Escreva o limite: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"S = {MinhaFuncao(n)}");
+        }
+        static double MinhaFuncao(double n)
+        {
+            int i;
+            double result = 0;
+            for (i = 1; i <= n; i++)
+            {
+                result += 1 / Fatorial(i);
+            }
+            return result;
+        }
+        static double Fatorial(double n)
+        {
+            double i;
+            for (i = n - 1; i > 0; i--)
+                n *= i;
+            return n;
+        }
+    }
+}
