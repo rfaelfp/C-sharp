d9a2855d19f42352038fbd643f4617fa556bf278
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-04-27 22:07:30 -0300

    27/04/2020,

--- Lista7Wesley/Questao7/Program.cs
+++ Lista7Wesley/Questao7/Program.cs
@@ -6,7 +6,7 @@ namespace Questao7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TESTE TESTE TESTE TESTE TEASTE");
         }
     }
 }
