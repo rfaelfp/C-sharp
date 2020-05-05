d351aa6c5697d124f9fd55f9411699053b41f40f
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-05-04 21:55:22 -0300

    04/05/2020

--- Lista7Wesley/Questao7/Program.cs
+++ Lista7Wesley/Questao7/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
-namespace Questao7
+class Programa
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("TESTE TESTE TESTE TESTE TEASTE");
-        }
+        int x = 15, y = 23;
+        string frase = "Fundamentos de Programação .. Indicador de Desempenho 01";
+        string resp = metodo(frase, x, y);
+        Console.WriteLine(resp);
+    }
+    static string metodo(string frase, int a, int b)
+    {
+        string result = "";
+        for (int i = a++; i < --b; i++)
+            result += frase[i];
+        return result;
     }
 }
--- questoesMaterial7/Questao1/Program.cs
+++ questoesMaterial7/Questao1/Program.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 
-namespace Questao1
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] vetor = new int[10];
+        int i;
+        for (i = 0; i < vetor.Length; i++)
         {
-            int[] vetor = new int[10];
-            int i;
-            for (i = 0; i < vetor.Length; i++)
-            {
-                Console.Write("Digite os valores do vetor: ");
-                vetor[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.WriteLine("Vetor:");
-            foreach (int e in vetor)
-            {
-                Console.Write(e + ", ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Vetor multiplicado por 3:");
-            for (i = 0; i < vetor.Length; i++)
-            {
-                vetor[i] *= 3;
-                Console.Write(vetor[i] + ", ");
-            }
+            Console.Write("Digite os valores do vetor: ");
+            vetor[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("Vetor:");
+        foreach (int e in vetor)
+        {
+            Console.Write(e + ", ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Vetor multiplicado por 3:");
+        for (i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] *= 3;
+            Console.Write(vetor[i] + ", ");
         }
     }
 }
