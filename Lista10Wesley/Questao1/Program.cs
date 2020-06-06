f50048ca06bf775a3b94ca2817f4d4f10f9998aa
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-06-06 13:59:31 -0300

    06/06/2020

--- Lista10Wesley/Questao1/Program.cs
+++ Lista10Wesley/Questao1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Channels;
 
 namespace Questao1
 {
@@ -8,40 +7,48 @@ namespace Questao1
     class Program
     {
         static void Main(string[] args)
-        {     
-            
-            int op = menu();
-            if (op == 0)
-            {
-                DadosAlunos[] aluno = new DadosAlunos[qtdAlunos()];
-                Cadastro cad = new Cadastro(aluno);
-                cad.IniciarVet(aluno);
-                cad.cadastro();
-                menu();
-            }
-            else if (op == 1)
-            {
-                
-                //cad.print();
-            }
-            else if (op == 4)
-                listar(op);
-            else if (op == 5)
-                listar(op);
-            else if (op == 6)
-                Environment.Exit(0);
-        }
-        static int qtdAlunos ()
         {
-            int qtd;
-            Console.Write("Digite a quantidade de alunos: ");
-            qtd = Convert.ToInt32(Console.ReadLine());
-            return qtd;
+            int op;
+            bool ver = false;
+            DadosAlunos[] aluno = new DadosAlunos[qtdAlunos()];
+            Cadastro cad = new Cadastro(aluno);
+            cad.IniciarVet(aluno);
+            do
+            {
+                op = menu();
+            if (op >= 0 && op <= 3)
+                {
+                    if (op == 0)
+                        cad.cadastro();
+                    else if (op == 1)
+                        cad.print();
+                }
+                    if (op == 4)
+                        listar(op);
+                    else if (op == 5)
+                        listar(op);
+                    else if (op == 6)
+                {
+                        ver = true;
+                        Environment.Exit(0);
+                }
+
+            }
+            while (ver == false);
+
+            static int qtdAlunos()
+            {
+                int qtd;
+                Console.Write("Digite a quantidade de alunos: ");
+                qtd = Convert.ToInt32(Console.ReadLine());
+                return qtd;
+            }
+
         }
-        static void listar (int op)
+        static void listar(int op)
         {
             if (op == 4)
-                foreach(var e in Enum.GetValues(typeof(Curso)))
+                foreach (var e in Enum.GetValues(typeof(Curso)))
                 {
                     Console.WriteLine(e);
                 }
@@ -53,7 +60,7 @@ namespace Questao1
                 }
             }
         }
-        static int menu ()
+        static int menu()
         {
             int opList = 0, opNum;
             Console.WriteLine("Sistema de cadastro de alunos.");
@@ -68,6 +75,5 @@ namespace Questao1
 
             return opNum;
         }
-        
     }
 }
