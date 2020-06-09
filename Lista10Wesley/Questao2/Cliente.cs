d043c788dcb81e1c7651e787e89ae74fe77ea47c
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-06-09 13:57:07 -0300

    Iniciando questão 2 da prova do Wesley

---
+++ Lista10Wesley/Questao2/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao2
+{
+    class Cliente
+    {
+
+    }
+}
---
+++ Lista10Wesley/Questao2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema Bancário");
+            AgenciaDados[] agencia = new AgenciaDados[5];
+            Agencia ag = new Agencia(agencia);
+            ag.iniciarVet();
+            //menu();
+            switch (menu())
+            {
+                case 1:
+                    ag.lerAgencia();
+                    break;
+                default:
+                    break;
+            }
+        }
+        static int menu()
+        {
+            int cont = 1, opGeral, opSubMenu = 0;
+            string[] opcao = { "Operações de agência", "Operação de conta", "Depósito/Transferência/Saldo" };
+            string[] opcaoAg = { "Incluir agência", "Excluir agência", "Imprimir dados agência", "Saldo do banco", "Saldo conta corrente", "Saldo conta poupança" };
+            for (int i = 0; i < opcao.Length; i++)
+            {
+                Console.WriteLine($"[{cont}] - {opcao[i]}");
+                cont++;
+            }
+            Console.Write("\nEscolha a opção: ");
+            opGeral = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (opGeral == 1)
+            {
+                cont = 1;
+                for (int i = 0; i < opcaoAg.Length; i++)
+                {
+                    Console.WriteLine($"[{cont}] - {opcaoAg[i]}");
+                    cont++;
+                }
+                Console.Write("\nEscolha a opção: ");
+                opSubMenu = Convert.ToInt32(Console.ReadLine());
+            }
+                return opSubMenu;
+        }
+    }
+}
